d9444ebd39c4674ca25d89c72dc6de68c8ecdc1b
Hygor <hygor_christian@hotmail.com>
2017-05-10 19:43:03 -0300

    Tentando criar um crud com o zend framework

---
+++ skeleton/module/Application/src/Model/ApresentaTable.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hygor Dias <hygor_christian@hotmail.com>
+ * Date: 10/05/2017
+ * Time: 19:18
+ */
+
+namespace Application\Model;
+
+
+class ApresentaTable {
+
+    private $tableGateway;
+
+    public function __construct() {
+    }
+
+}
